2835382ad0d00951f45a37a2059c5c112855527b
David Rönnlid <david.ronnlid@gmail.com>
2023-10-12 18:48:04 +0200

    insertmany op started

--- netlify/functions/getFLData.ts
+++ netlify/functions/getFLData.ts
@@ -1,6 +1,80 @@
 import { MongoClient } from "mongodb";
+const { v4: uuidv4 } = require("uuid");
 
 const uri: string = process.env.MONGODB_URI!;
+
+const lectureDetails = [
+  {
+    id: uuidv4(),
+    title: "Transplantation",
+    lecturer: "Olle Korsgren",
+    date: "2023-10-16",
+    time: "13:15 - 17:00",
+  },
+  {
+    id: uuidv4(),
+
+    title: "Autoimmunitetsdiagnostik",
+    lecturer: "Johan Rönnelid",
+    date: "2023-10-17",
+    time: "13:15 - 17:00",
+  },
+  {
+    id: uuidv4(),
+
+    title: "Morfologi vid njursjukdomar och njurtransplantation",
+    lecturer: "Anca Dragomir",
+    date: "2023-10-18",
+    time: "09:15 - 12:00",
+  },
+  {
+    id: uuidv4(),
+
+    title: "Klinisk handläggning av allergi",
+    lecturer: "Christer Janson",
+    date: "2023-10-19",
+    time: "09:15 - 11:00",
+  },
+  {
+    id: uuidv4(),
+
+    title: "Immunbrist",
+    lecturer: "Torsten Eich",
+    date: "2023-10-19",
+    time: "12:15 - 14:00",
+  },
+  {
+    id: uuidv4(),
+
+    title: "Biomaterial",
+    lecturer: "Karin Fromell",
+    date: "2023-10-19",
+    time: "14:15 - 16:00",
+  },
+  {
+    id: uuidv4(),
+
+    title: "Typ 1 Diabetes",
+    lecturer: "Olle Korsgren",
+    date: "2023-10-20",
+    time: "10:15 - 12:00",
+  },
+  {
+    id: uuidv4(),
+
+    title: "Samspelet mellan medfödd och förvärvad immunitet",
+    lecturer: "Peter Seiron",
+    date: "2023-10-24",
+    time: "13:15 - 15:00",
+  },
+  {
+    id: uuidv4(),
+
+    title: "Frågestund patologi",
+    date: "2023-10-24",
+    time: "14:15 - 16:00",
+  },
+];
 
 export const handler = async (event: any, context: any): Promise<any> => {
   const client: MongoClient = new MongoClient(uri);
@@ -14,9 +88,7 @@ export const handler = async (event: any, context: any): Promise<any> => {
     const collection = database.collection("forelasningsdata");
 
     console.log("Attempting to fetch data...");
-    const FLData = await collection.findOne({
-      id: "65281ffb177959af1ebb987c",
-    });
+    const FLData = await collection.insertMany(lectureDetails);
 
     console.log("Data fetched: ", FLData);
 
--- src/data/lectureData.ts
+++ src/data/lectureData.ts
@@ -1,71 +1,56 @@
 const lectureDetails = [
   {
-    week: "Kursvecka 8",
-    lectures: [
-      {
-        title: "Transplantation",
-        lecturer: "Olle Korsgren",
-        date: "2023-10-16",
-        time: "13:15 - 17:00",
-      },
-      {
-        title: "Autoimmunitetsdiagnostik",
-        lecturer: "Johan Rönnelid",
-        date: "2023-10-17",
-        time: "13:15 - 17:00",
-      },
-      {
-        title: "Morfologi vid njursjukdomar och njurtransplantation",
-        lecturer: "Anca Dragomir",
-        date: "2023-10-18",
-        time: "09:15 - 12:00",
-      },
-      {
-        title: "Klinisk handläggning av allergi",
-        lecturer: "Christer Janson",
-        date: "2023-10-19",
-        time: "09:15 - 11:00",
-      },
-      {
-        title: "Immunbrist",
-        lecturer: "Torsten Eich",
-        date: "2023-10-19",
-        time: "12:15 - 14:00",
-      },
-      {
-        title: "Biomaterial",
-        lecturer: "Karin Fromell",
-        date: "2023-10-19",
-        time: "14:15 - 16:00",
-      },
-      {
-        title: "Typ 1 Diabetes",
-        lecturer: "Olle Korsgren",
-        date: "2023-10-20",
-        time: "10:15 - 12:00",
-      },
-    ],
+    title: "Transplantation",
+    lecturer: "Olle Korsgren",
+    date: "2023-10-16",
+    time: "13:15 - 17:00",
   },
   {
-    week: "Kursvecka 9",
-    lectures: [
-      {
-        title: "Samspelet mellan medfödd och förvärvad immunitet",
-        lecturer: "Peter Seiron",
-        date: "2023-10-24",
-        time: "13:15 - 15:00",
-      },
-    ],
+    title: "Autoimmunitetsdiagnostik",
+    lecturer: "Johan Rönnelid",
+    date: "2023-10-17",
+    time: "13:15 - 17:00",
   },
   {
-    week: "Kursvecka 10",
-    lectures: [
-      {
-        title: "Frågestund patologi",
-        date: "2023-10-24",
-        time: "14:15 - 16:00",
-      },
-    ],
+    title: "Morfologi vid njursjukdomar och njurtransplantation",
+    lecturer: "Anca Dragomir",
+    date: "2023-10-18",
+    time: "09:15 - 12:00",
+  },
+  {
+    title: "Klinisk handläggning av allergi",
+    lecturer: "Christer Janson",
+    date: "2023-10-19",
+    time: "09:15 - 11:00",
+  },
+  {
+    title: "Immunbrist",
+    lecturer: "Torsten Eich",
+    date: "2023-10-19",
+    time: "12:15 - 14:00",
+  },
+  {
+    title: "Biomaterial",
+    lecturer: "Karin Fromell",
+    date: "2023-10-19",
+    time: "14:15 - 16:00",
+  },
+  {
+    title: "Typ 1 Diabetes",
+    lecturer: "Olle Korsgren",
+    date: "2023-10-20",
+    time: "10:15 - 12:00",
+  },
+  {
+    title: "Samspelet mellan medfödd och förvärvad immunitet",
+    lecturer: "Peter Seiron",
+    date: "2023-10-24",
+    time: "13:15 - 15:00",
+  },
+  {
+    title: "Frågestund patologi",
+    date: "2023-10-24",
+    time: "14:15 - 16:00",
   },
 ];
 
