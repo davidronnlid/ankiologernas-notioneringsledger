2ab89c9af3101cc6af97f9c44df5a45aab9bdc9b
David Rönnlid <david.ronnlid@gmail.com>
2023-12-10 18:13:25 +0100

    loading spinners added back to the app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
-import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { RootState } from "../store/types";
 import Layout from "@/components/Layout";
 import LectureTitle from "@/components/LectureTitle";
 import { WeekData } from "@/types";
 import Table from "@/components/Table";
-import { Button, Typography } from "@material-ui/core";
+import { Button, CircularProgress, Typography } from "@material-ui/core";
 import React from "react";
 import Link from "next/link";
 import FlashOnSharpIcon from "@mui/icons-material/FlashOnSharp";
@@ -15,59 +14,73 @@ export default function Index() {
   const lecturesData = useSelector(
     (state: RootState) => state.lectures.lectures
   );
-  console.log("🚀 ~ file: index.tsx:37 ~ Index ~ lecturesData:", lecturesData);
+
+  const isLoading = !lecturesData || lecturesData.length === 0;
 
   return (
     <Layout>
-      <>
-        {coursePeriods.map((course) => {
-          // Find all weeks that belong to this course
-          const courseWeeks = lecturesData.filter(
-            (weekData: WeekData) => weekData.course === course.title
-          );
+      {isLoading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "start",
+            height: "80vh",
+          }}
+        >
+          <CircularProgress />
+        </div>
+      ) : (
+        <>
+          {coursePeriods.map((course) => {
+            // Find all weeks that belong to this course
+            const courseWeeks = lecturesData.filter(
+              (weekData: WeekData) => weekData.course === course.title
+            );
 
-          return (
-            <>
-              {/* Render the course title once */}
-              {courseWeeks.length > 0 && (
-                <>
-                  <Typography
-                    variant="h4"
-                    style={{ marginBottom: "3rem" }}
-                    gutterBottom
-                  >
-                    {course.title}{" "}
-                    <Link href="/lecture-list" passHref>
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        style={{
-                          background: "transparent",
-                          boxShadow: "none",
-                          color: "white",
-                          width: "3rem",
-                        }}
-                      >
-                        <FlashOnSharpIcon />
-                      </Button>
-                    </Link>
-                  </Typography>{" "}
-                </>
-              )}
+            return (
+              <>
+                {/* Render the course title once */}
+                {courseWeeks.length > 0 && (
+                  <>
+                    <Typography
+                      variant="h4"
+                      style={{ marginBottom: "3rem" }}
+                      gutterBottom
+                    >
+                      {course.title}{" "}
+                      <Link href="/lecture-list" passHref>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          style={{
+                            background: "transparent",
+                            boxShadow: "none",
+                            color: "white",
+                            width: "3rem",
+                          }}
+                        >
+                          <FlashOnSharpIcon />
+                        </Button>
+                      </Link>
+                    </Typography>{" "}
+                  </>
+                )}
 
-              {/* Then render all weeks that belong to this course */}
-              {courseWeeks.map((weekData: WeekData) => (
-                <LectureTitle
-                  key={weekData.week}
-                  week={weekData.week}
-                  lectures={weekData.lectures}
-                />
-              ))}
-            </>
-          );
-        })}
-        <Table />
-      </>
+                {/* Then render all weeks that belong to this course */}
+                {courseWeeks.map((weekData: WeekData) => (
+                  <LectureTitle
+                    key={weekData.week}
+                    week={weekData.week}
+                    lectures={weekData.lectures}
+                  />
+                ))}
+              </>
+            );
+          })}
+          <Table />
+        </>
+      )}
     </Layout>
   );
 }
--- src/pages/lecture-list.tsx
+++ src/pages/lecture-list.tsx
@@ -7,6 +7,7 @@ import {
   Box,
   Button,
   Paper,
+  CircularProgress,
 } from "@material-ui/core";
 import Lecture from "types/lecture";
 import VemNotionerar from "@/components/VemNotionerar";
@@ -21,129 +22,114 @@ const courseTitle = "Medicinsk Mikrobiologi";
 
 export default function LectureList() {
   const weeksData = useSelector((state: RootState) => state.lectures.lectures);
-  console.log(
-    "🚀 ~ file: lecturelist.tsx:73 ~ lecturelist ~ weeksData:",
-    weeksData
-  );
 
   let lectureGlobalIndex = 0;
+  const isLoading = !weeksData || weeksData.length === 0;
 
   return (
     <Layout>
-      <>
-        <Typography variant="h4" gutterBottom style={{ marginBottom: "3rem" }}>
-          {courseTitle}{" "}
-          <Link href="/" passHref>
-            <Button
-              style={{
-                background: "transparent",
-                boxShadow: "none",
-                color: "white",
-                width: "3rem",
-              }}
-              variant="contained"
-              color="primary"
-              size="large"
-            >
-              <ZoomInSharpIcon />
-            </Button>
-          </Link>
-        </Typography>
-
-        <Box
-          width="100%"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          style={{ marginTop: "3rem" }}
+      {isLoading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "start",
+            height: "80vh",
+          }}
         >
-          <Grid container spacing={1} style={{ maxWidth: "100%" }}>
-            {weeksData.map((week) =>
-              week.lectures.map((lecture: Lecture) => {
-                // Increment the global index for each lecture
-                lectureGlobalIndex += 1;
-                const bgColor = setLectureBgColor(lecture.date);
+          <CircularProgress />
+        </div>
+      ) : (
+        <>
+          <Typography
+            variant="h4"
+            gutterBottom
+            style={{ marginBottom: "3rem" }}
+          >
+            {courseTitle}{" "}
+            <Link href="/" passHref>
+              <Button
+                style={{
+                  background: "transparent",
+                  boxShadow: "none",
+                  color: "white",
+                  width: "3rem",
+                }}
+                variant="contained"
+                color="primary"
+                size="large"
+              >
+                <ZoomInSharpIcon />
+              </Button>
+            </Link>
+          </Typography>
 
-                return (
-                  <Grid
-                    key={lectureGlobalIndex}
-                    item
-                    xs={12}
-                    sm={4}
-                    md={3}
-                    lg={2}
-                  >
-                    <Paper
-                      style={{
-                        padding: "10px",
-                        paddingTop: "30px",
-                        backgroundColor: bgColor,
-                        color: "white",
-                      }}
+          <Box
+            width="100%"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            style={{ marginTop: "3rem" }}
+          >
+            <Grid container spacing={1} style={{ maxWidth: "100%" }}>
+              {weeksData.map((week) =>
+                week.lectures.map((lecture: Lecture) => {
+                  // Increment the global index for each lecture
+                  lectureGlobalIndex += 1;
+                  const bgColor = setLectureBgColor(lecture.date);
+
+                  return (
+                    <Grid
+                      key={lectureGlobalIndex}
+                      item
+                      xs={12}
+                      sm={4}
+                      md={3}
+                      lg={2}
                     >
-                      <TooltipComponent
-                        text={
-                          <React.Fragment>
-                            <Typography color="inherit">
-                              {lecture.title}
-                            </Typography>
-
-                            <Typography variant="body2">
-                              <em>Datum:</em> {lecture.date}
-                            </Typography>
-                            <Typography variant="body2">
-                              <em>Tid:</em> {lecture.time}
-                            </Typography>
-                            <Typography variant="body2">
-                              <em>Föreläsare:</em> {lecture.lecturer}
-                            </Typography>
-                          </React.Fragment>
-                        }
+                      <Paper
+                        style={{
+                          padding: "10px",
+                          paddingTop: "30px",
+                          backgroundColor: bgColor,
+                          color: "white",
+                        }}
                       >
-                        {lectureGlobalIndex}
-                      </TooltipComponent>
+                        <TooltipComponent
+                          text={
+                            <React.Fragment>
+                              <Typography color="inherit">
+                                {lecture.title}
+                              </Typography>
 
-                      {/* <Tooltip
-                        title={
-                          <React.Fragment>
-                            <Typography color="inherit">
-                              {lecture.title}
-                            </Typography>
-
-                            <Typography variant="body2">
-                              <em>Datum:</em> {lecture.date}
-                            </Typography>
-                            <Typography variant="body2">
-                              <em>Tid:</em> {lecture.time}
-                            </Typography>
-                            <Typography variant="body2">
-                              <em>Föreläsare:</em> {lecture.lecturer}
-                            </Typography>
-                          </React.Fragment>
-                        }
-                        placement="top"
-                        arrow
-                      >
-                        <Typography
-                          variant="body2"
-                          style={{ cursor: "pointer", margin: "0 20px 25px 0" }}
+                              <Typography variant="body2">
+                                <em>Datum:</em> {lecture.date}
+                              </Typography>
+                              <Typography variant="body2">
+                                <em>Tid:</em> {lecture.time}
+                              </Typography>
+                              <Typography variant="body2">
+                                <em>Föreläsare:</em> {lecture.lecturer}
+                              </Typography>
+                            </React.Fragment>
+                          }
                         >
                           {lectureGlobalIndex}
-                        </Typography>
-                      </Tooltip> */}
+                        </TooltipComponent>
 
-                      <VemNotionerar
-                        lectureID={lecture.id}
-                        checkboxState={lecture.checkboxState}
-                      />
-                    </Paper>
-                  </Grid>
-                );
-              })
-            )}
-          </Grid>
-        </Box>
-      </>
+                        <VemNotionerar
+                          lectureID={lecture.id}
+                          checkboxState={lecture.checkboxState}
+                        />
+                      </Paper>
+                    </Grid>
+                  );
+                })
+              )}
+            </Grid>
+          </Box>
+        </>
+      )}
     </Layout>
   );
 }
